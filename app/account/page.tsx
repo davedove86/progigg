4278c0d421dbbb2b9f01b640f867d345092ba879
Dave Dove <info@dovedesign.io>
2024-11-11 13:47:32 +0000

    sidebar and a few pages created

---
+++ app/account/page.tsx
@@ -0,0 +1,36 @@
+'use client';
+import { data } from '@/components/app-sidebar';
+import Image from 'next/image';
+
+export default function Page() {
+  const user = data.user;
+
+  return (
+    <div className='p-4'>
+      <h1 className='text-2xl'>Account</h1>
+      <div className='bg-slate-100 flex flex-col my-6 rounded-sm p-4'>
+        <Image
+          className='h-16 w-16 rounded-full mb-6'
+          src={user.avatar}
+          alt={user.name}
+          width={100}
+          height={100}
+        />
+        <div className='flex flex-col gap-2'>
+          <p>
+            <span className='font-bold'>Name: </span>
+            {user.name}
+          </p>
+          <p>
+            <span className='font-bold'>Email: </span>
+            {user.email}
+          </p>
+          <p>
+            <span className='font-bold'>Skills: </span>
+            {user.skills.join(', ')}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,60 +1,113 @@
-import { AppSidebar } from '@/components/app-sidebar';
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb';
-import { Separator } from '@/components/ui/separator';
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from '@/components/ui/sidebar';
-
 export default function Page() {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className='flex h-16 shrink-0 items-center gap-2'>
-          <div className='flex items-center gap-2 px-4'>
-            <SidebarTrigger className='-ml-1' />
-            <Separator orientation='vertical' className='mr-2 h-4' />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className='hidden md:block'>
-                  <BreadcrumbLink href='#'>
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className='hidden md:block' />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
-          <div className='grid auto-rows-min gap-4 md:grid-cols-3'>
-            <div className='aspect-video rounded-xl bg-muted/50'>
-              <h2>Col 1</h2>
+    <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
+      <div className='grid auto-rows-min gap-4 md:grid-cols-2'>
+        <div className='aspect-video rounded-xl bg-muted/50 p-2'>
+          <h2 className='text-xl mb-4'>Applied Giggs</h2>
+          <ul className='flex flex-col gap-6'>
+            <div className='flex justify-between'>
+              <li>Front End Developer</li>
+              <span className='bg-green-600 rounded-md text-xs text-white font-bold py-1 px-2'>
+                Recieved
+              </span>
             </div>
-            <div className='aspect-video rounded-xl bg-muted/50'>
-              <h2>Col 2</h2>
+            <div className='flex justify-between'>
+              <li>Webflow Developer</li>
+              <span className='bg-orange-600 rounded-md text-xs text-white font-bold py-1 px-2'>
+                Sent
+              </span>
             </div>
-            <div className='aspect-video rounded-xl bg-muted/50'>
-              <h2>Col 3</h2>
+            <div className='flex justify-between'>
+              <li>Back End Developer</li>
+              <span className='bg-purple-600 rounded-md text-xs text-white font-bold py-1 px-2'>
+                In Review
+              </span>
             </div>
-          </div>
-          <div className='min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min'>
-            <h2>Col 4</h2>
-          </div>
+            <div className='flex justify-between'>
+              <li>Front End Developer</li>
+              <span className='bg-green-600 rounded-md text-xs text-white font-bold py-1 px-2'>
+                Recieved
+              </span>
+            </div>
+            <div className='flex justify-between'>
+              <li>Front End Developer</li>
+              <span className='bg-green-600 rounded-md text-xs text-white font-bold py-1 px-2'>
+                Recieved
+              </span>
+            </div>
+          </ul>
         </div>
-      </SidebarInset>
-    </SidebarProvider>
+
+        <div className='aspect-video rounded-xl bg-muted/50 p-2'>
+          <h2 className='text-xl mb-4'>Messages</h2>
+        </div>
+      </div>
+      <div className='min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min p-2'>
+        <h2 className='text-xl mb-4'>Available Gigs</h2>
+        <div className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4'>
+          {/* Headings */}
+          <div className='font-bold'>Gigg Title</div>
+          <div className='font-bold'>Company</div>
+          <div className='font-bold'>Skills</div>
+          <div className='font-bold'>Status</div>
+          {/* list items */}
+          <p>Front End Developer</p>
+          <p>Google</p>
+          <p>React, TypeScript</p>
+          <button className='bg-blue-600 rounded-md text-xs text-white font-bold py-1 px-2'>
+            Apply for Gigg
+          </button>
+          {/* list items */}
+          <p>Front End Developer</p>
+          <p>Google</p>
+          <p>React, TypeScript</p>
+          <button className='bg-blue-600 rounded-md text-xs text-white font-bold py-1 px-2'>
+            Apply for Gigg
+          </button>
+          {/* list items */}
+          <p>Front End Developer</p>
+          <p>Google</p>
+          <p>React, TypeScript</p>
+          <button className='bg-blue-600 rounded-md text-xs text-white font-bold py-1 px-2'>
+            Apply for Gigg
+          </button>
+          {/* list items */}
+          <p>Front End Developer</p>
+          <p>Google</p>
+          <p>React, TypeScript</p>
+          <button className='bg-blue-600 rounded-md text-xs text-white font-bold py-1 px-2'>
+            Apply for Gigg
+          </button>
+          {/* list items */}
+          <p>Front End Developer</p>
+          <p>Google</p>
+          <p>React, TypeScript</p>
+          <button className='bg-blue-600 rounded-md text-xs text-white font-bold py-1 px-2'>
+            Apply for Gigg
+          </button>
+          {/* list items */}
+          <p>Front End Developer</p>
+          <p>Google</p>
+          <p>React, TypeScript</p>
+          <button className='bg-blue-600 rounded-md text-xs text-white font-bold py-1 px-2'>
+            Apply for Gigg
+          </button>
+          {/* list items */}
+          <p>Front End Developer</p>
+          <p>Google</p>
+          <p>React, TypeScript</p>
+          <button className='bg-blue-600 rounded-md text-xs text-white font-bold py-1 px-2'>
+            Apply for Gigg
+          </button>
+          {/* list items */}
+          <p>Front End Developer</p>
+          <p>Google</p>
+          <p>React, TypeScript</p>
+          <button className='bg-blue-600 rounded-md text-xs text-white font-bold py-1 px-2'>
+            Apply for Gigg
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
